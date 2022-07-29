01973adf6652b0d2c4a8329b0a6e4c5ae6c88665
wanghaichen <wanghaichen@hcses.com>
2022-07-29 09:29:39 +0800

    w

---
+++ memory/src/test/java/icu/javaking/way/memory/MemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.javaking.way.memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
